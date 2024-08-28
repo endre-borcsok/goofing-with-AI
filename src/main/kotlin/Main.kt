aeb88358f2a8e33c40fed0d579ebae2aa03db984
Endre Borcsok <endre.borcsok@videoloft.com>
2024-08-28 19:27:07 +0100

    Add some robustness

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,7 +15,10 @@ import model.WorldModel
 import model.`object`.ObjectModel
 import usecase.UpdateWorldUseCase
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
+import java.lang.Thread.sleep
+import java.net.Socket
 
 const val serverLaunchCommand = "./ai_launch.sh"
 
@@ -61,8 +64,17 @@ private fun WorldModel.over() =
 private fun CoroutineScope.startServer() {
     val process = Runtime.getRuntime().exec(serverLaunchCommand)
     println(process.info())
-    val reader = BufferedReader(InputStreamReader(process.inputStream))
+    while (true) {
+        try {
+            Socket("127.0.0.1", 9999)
+            break
+        } catch (e: Exception) {
+            println(e)
+            sleep(1000)
+        }
+    }
     launch {
+        val reader = BufferedReader(InputStreamReader(process.inputStream))
         for (line in reader.lines()) {
             println(line)
         }
